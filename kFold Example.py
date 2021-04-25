211f953caf6dc55aaccc0787a1756fc603c2fa55
JGurth <j.gurth@outlook.de>
2021-04-25 13:32:52 +0200

    CEsembleNet

--- kFold Example.py
+++ kFold Example.py
@@ -8,9 +8,11 @@ from lookatthisgraph.utils.dataset import Dataset
 from lookatthisgraph.utils.trainer import Trainer
 from lookatthisgraph.nets.ConvNet import ConvNet
 from lookatthisgraph.nets.ChebConv2 import ChebConvNet
+from lookatthisgraph.nets.CEnsembleNet1 import CEnsembleNet1
 from lookatthisgraph.nets.EnsembleNet1 import EnsembleNet1
 from lookatthisgraph.nets.EnsembleNet import EnsembleNet
 from lookatthisgraph.nets.EnsembleNet3 import EnsembleNet3
+from lookatthisgraph.nets.CEnsembleNet import CEnsembleNet
 from lookatthisgraph.nets.PointConv import PointNet
 import datetime as dt
 
@@ -33,7 +35,7 @@ train_config = {
         'max_epochs': 60,
         'kFold_max' : k_max,
         'kFold_size' : k_size,
-        'net': PointNet,
+        'net': CEnsembleNet1,
         'dataset': train_set
     }
               
@@ -70,14 +72,14 @@ for k_crnt in range(k_max):
     endresult0=torch.cat(resultlist, 0)  
     
     if SaveNet:
-        trainer.save_network_info("Results/EnsembleSmall/Net_"+train_config['net'](1,1).__class__.__name__+"_"+train_config['training_target']+"_"+str(avrg)+".p")
+        trainer.save_network_info("Results/CEnsemble/Net_"+train_config['net'](1,1).__class__.__name__+"_"+train_config['training_target']+"_"+str(avrg)+".p")
 
     
 
 endresult=torch.mean(endresult0).item()
 STD=torch.std(endresult0).item()
 print('k-Fold final Accuracy:', endresult)
-filename="Results/EnsembleSmall/Acc_"+train_config['net'](1,1).__class__.__name__+"_"+train_config['training_target']+"_"+dt.datetime.now().strftime("%d-%m-%Y_%H-%M")+".txt"
+filename="Results/CEnsemble/Acc_3"+train_config['net'](1,1).__class__.__name__+"_"+train_config['training_target']+"_"+dt.datetime.now().strftime("%d-%m-%Y_%H-%M")+".txt"
 file=open(filename, "w")
 file.writelines(['k-Fold final Accuracy: '+str(endresult)+"\n", 'k-Fold standart deviation: '+str(STD)+"\n", "k_max="+str(k_max)+"\n", "k_size="+str(k_size)+"\n", "Epochs="+str(train_config['max_epochs'])+"\n", "Batch_Size="+str(train_config['batch_size'])+"\n", "Time="+str(time)])
 file.close()
